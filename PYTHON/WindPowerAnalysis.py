1a27873c6c75bc4ce0c0dd5cf4b7f24424d86c86
Gaetano Montessuto <168757686+BroodJust@users.noreply.github.com>
2024-06-11 15:25:10 +0200

    Add files via upload

--- PYTHON/WindPowerAnalysis.py
+++ PYTHON/WindPowerAnalysis.py
@@ -1,6 +1,6 @@
 import math
 contatore=0
-infile= open('Datawindwavetab_Tahiti.csv','r')
+infile= open('Datawindwavetab_SanPietro.csv','r')
 line=infile.readline()
 diz={}
 data= []
@@ -46,16 +46,21 @@ max_pwr= float(input('Insert rated power:'))
 sp_rate= float(input('Insert rated wind speed:'))
 
 for d in range(0,len(data)):
-    Flag=True
-    apprx=round(data[d]['wind speed'],0)                        #inserisco il cp giusto per ogni velocità del vento
+    Flag=True    
+    act_speed=round(data[d]['wind speed'],2) 
+    sto=0                        #inserisco il cp giusto per ogni velocità del vento
     for t in range(0,len(cp_data)):                             #ATTENZIONE: APPROSSIMAZIONE CP dell'ordine dell'unità (n) poiche vi è scostamento minimo con termini del tipo n.5
-        glt=cp_data[t]['speed']      
-        if apprx==cp_data[t]['speed']:
-            if apprx!=sp_rate:
-                data[d]['correct cp']=cp_data[t]['Cp_value']
-            elif apprx==sp_rate:
+        glt=round(cp_data[t]['speed'],2)
+        if sto<=act_speed and act_speed<=glt:            
+            val_medio= round(((glt-sto)*0.5) +sto,2)
+            #if l!=sp_rate:
+            if act_speed<=val_medio:
                 data[d]['correct cp']=prec['Cp_value']
-            
+            elif act_speed> val_medio:
+                data[d]['correct cp']=cp_data[t]['Cp_value']
+            #elif l==sp_rate:
+                #data[d]['correct cp']=prec['Cp_value']
+        sto=glt    
         prec=cp_data[t]
 
 
@@ -64,6 +69,7 @@ sp_out= float(input('Insert cut out wind speed:'))
 raggio= int(input('Insert Rotor Diameter:'))/2
 rho= 1.225
 pi= math.pi
+somma=0
 
 for l in range(0,len(data)):
     an=data[l]
@@ -84,6 +90,12 @@ for l in range(0,len(data)):
         cf= pwr/max_pwr
     an['Power']=pwr
     an['Capacity factor']=cf
+    somma+=cf
+    
+media= somma/len(data)
+print('Capacity Factor:',media)
+
+
 
 import csv          #scrivo il csv
 import os
@@ -92,7 +104,7 @@ output_dir = 'output'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-with open(os.path.join(output_dir, 'San_Pietro_power_data15MWh.csv'), 'w', newline='') as outfile:
+with open(os.path.join(output_dir, 'SanPietro_power_data1.5MW.csv'), 'w', newline='') as outfile:
     fieldnames = ['Date,Time', 'wind speed', 'Power','correct cp','Capacity factor']
     writer = csv.DictWriter(outfile, fieldnames=fieldnames)
 
@@ -107,3 +119,4 @@ with open(os.path.join(output_dir, 'San_Pietro_power_data15MWh.csv'), 'w', newli
 
 
 
+#valore sopra- valore sotto /2 -+ valore sotto
